54d4c939169edb8134dcb5eabc5f2573012c3eea
haemmian <haemmig.a@gmail.com>
2022-11-14 12:33:45 +0100

    commit Task1-3

--- src/car.cpp
+++ src/car.cpp
@@ -1,2 +1,55 @@
+#include "car.h"
+
+Car::Car()
+{
+
+}
+
+Car::Car(int value, int capacity,int power, int number, std::string color)
+{
+    this->value               = value;
+    this->capacity            = capacity;
+    this->power               = power;
+    this->number              = number;
+    this->color              = color;
+
+    //std::cout << "Car informations:\nValue: $" << value << "\nCapacity: " << capacity << "\nPower: " << power << "\nNumber: " << number <<
+    //             "\nColour: " << clr << std::endl;
+}
 
 
+//GetValues for the car
+int Car::GetNumber()
+{
+    return number;
+}
+
+int Car::GetValue()
+{
+    return value;
+}
+
+int Car::GetCapacity()
+{
+    return capacity;
+}
+
+int Car::GetPower()
+{
+    return power;
+}
+
+std::string Car::GetColor()
+{
+    return color;
+}
+
+
+
+Race_car::Race_car(int value, int capacity,int power, int number, std::string color, int top_speed): Car(value, capacity, power, number, color)
+{
+
+    mTop_speed = top_speed;
+
+    //std::cout << "Top speed: " << top_speed << "km/h" << std::endl;
+}
--- src/warehouse.h
+++ src/warehouse.h
@@ -1,24 +1,38 @@
 #ifndef WAREHOUSE_H
 #define WAREHOUSE_H
+#include "car.h"
+
+
 #include <iostream>
+#define MAX 1000
+
 
 class Warehouse
 {
-private:
-    int capacity, index[10], cnt_Car = 0; //index refers to parking slot
-public:
-    Warehouse(int cp)
-    {
-        capacity = cp;
 
-        //std::cout<<  "Warehouse\nCapacity: " << capacity << std::endl;
-    }
-    void parking();
-    void leaving(int idxOUT);
+private:
+    int mCapacity, index, cnt_Car = 0, Car_Number; //index refers to parking slot
+    int value_car, capacity_car, power_car, number_car[10]; //the first     [] is used for the input value
+
+    Car Storage[MAX];
+
+    //Private Functions
+    int sortNumber();
+
+
+public:
+
+
+    Warehouse(int capacity);
+    void parking(Car car);
+    void leaving(int index_out);
     int numbCars();
     int Capacity();
+
+    int returnCars();
 
 };
 
 
 #endif // WAREHOUSE_H
+
